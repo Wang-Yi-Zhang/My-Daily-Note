dd78ef1f8f9f1e1ab5abcdb096f7c33fd6d24071
A4 <hemoparasite@gmail.com>
2026-01-18 23:00:37 +0800

    Update API URL in configuration for deployment

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
   API_URL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
         ? 'http://localhost:3000/api'  // 本地開發用的後端地址
-        : 'https://你的專案名稱.zeabur.app/api', // 請填入你在 Zeabur 部署後的後端網址
+        : 'https://my-daily-note.zeabur.app/api', // 請填入你在 Zeabur 部署後的後端網址
 
   DEFAULT_CATEGORIES: [
         { name: '工作', color: '#ffadad', target: 20 },
